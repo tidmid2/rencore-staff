513033603cea4af9d5d93f8bacb0dd862ee5cf4e
tidmid2 <72195488+tidmid2@users.noreply.github.com>
2022-12-15 16:54:05 +0600

    123

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,38 +1,6 @@
-// import React, { useState, useEffect } from "react";
-// import { usePosition } from "use-position";
-// import axios from "axios";
 import { Typography, Box } from "@mui/material";
 
-// eslint-disable-next-line 
-const geolocation = [
-  //43.268875~43.268112
-  //76.934728~76.935567
-  { lat: "43.269", lon: "76.934" },
-  { lat: "43.269", lon: "76.934" },
-  { lat: "43.268", lon: "76.936" },
-  { lat: "43.268", lon: "76.936" },
-];
-
 function Main() {
-  // const [isVisible, setIsVisible] = useState(true);
-  // // const { latitude, longitude } = usePosition();
-  // const [ip, setIP] = useState("");
-
-  // const geoMap =  () => {
-  //   let lat = "43.26856"; let lon = "76.935";
-    
-  //   if(lat<=geolocation[0].lat && lat>=geolocation[3].lat){
-  //     if(lon>=geolocation[0].lon && lon<=geolocation[3].lon){
-  //       return true;
-  //     }
-  //     else{
-  //       return;
-  //     }
-  //   } else{
-  //     return;
-  //   }
-  // };
-
   return (
     <Typography component="div">
       <Typography component="h1" variant="h4" align="center">
@@ -57,14 +25,6 @@ function Main() {
         Для работы над программей войдите в учетную запись или
         зарегестрируйтесь.
       </Box>
-
-      {/* <Box>
-        latitude: {latitude}
-        <br />
-        longitude: {longitude}
-        <br />
-        {/* IP: {ip}<br/> */}
-      {/* </Box> */} 
     </Typography>
   );
 }
